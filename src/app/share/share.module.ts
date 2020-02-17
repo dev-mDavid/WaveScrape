7a5024fb2c81c0470f20c7141e47b46e23f8ff28
mDavid <dev.mdavid@gmail.com>
2020-02-16 17:00:24 -0800

    config share module

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+const components = [];
 
-
+const modules = [
+  CommonModule,
+];
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [...components],
+  imports: [ ...modules ],
+  exports: [...components, ...modules]
 })
 export class ShareModule { }
