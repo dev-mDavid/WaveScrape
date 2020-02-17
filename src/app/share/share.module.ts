6f7a6a073c51ff5b41ba44f09ed6c9e0a02dfcc0
mDavid <dev.mdavid@gmail.com>
2020-02-16 17:59:21 -0800

    style nav shell

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ShellComponent } from '../shell/shell.component';
 
 import { MatToolbarModule } from "@angular/material/toolbar";
-
+import { MatIconModule } from "@angular/material/icon";
 const components = [ShellComponent];
 
 const modules = [
   CommonModule,
   MatToolbarModule,
+  MatIconModule,
 ];
 @NgModule({
   declarations: [...components],
