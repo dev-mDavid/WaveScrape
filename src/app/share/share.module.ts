f5bfc196201456473a68de492033e18625531cb8
mDavid <dev.mdavid@gmail.com>
2020-02-21 19:21:48 -0800

    add material menu module to share module

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -5,6 +5,7 @@ import { ShellComponent } from '../shell/shell.component';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { MmtListItemComponent } from '../mmt-list-item/mmt-list-item.component';
+import { MatMenuModule } from "@angular/material/menu";
 
 const components = [
   ShellComponent,
@@ -15,6 +16,7 @@ const modules = [
   CommonModule,
   MatToolbarModule,
   MatIconModule,  
+  MatMenuModule
 ];
 @NgModule({
   declarations: [...components],
