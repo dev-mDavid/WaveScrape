4f25a6a938c4ec29ad6cd2a1ae91dde446dec3d7
mDavid <dev.mdavid@gmail.com>
2020-02-21 17:19:23 -0800

    dev the logic for the device responsive service

--- src/app/share/device-responsive.service.ts
+++ src/app/share/device-responsive.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeviceResponsiveService {
 
-  constructor() { }
+  /* Makes the device size into an observable for dynamic responsiveness */     
+  whatDevice(deviceType: string): Observable<boolean> {
+    return this.breakpointObserver.observe(deviceType)
+      .pipe(
+        map(result => result.matches),
+        shareReplay()
+      );
+  }
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
 }
+
+  /* How to Use 
+   *
+  */
