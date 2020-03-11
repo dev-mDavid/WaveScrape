1de3d36850c8909b3d82ea5fe1714ff2964d579e
mDavid <dev.mdavid@gmail.com>
2020-03-11 15:58:02 -0700

    create mock object for current data item

--- src/app/current-page/current-components/current-data-item__list/current-data-item.component__list.ts
+++ src/app/current-page/current-components/current-data-item__list/current-data-item.component__list.ts
@@ -11,6 +11,62 @@ export class CurrentDataItemListComponent implements OnInit {
   //   name: string;
   // }
 
+  breaksCurrent = [
+    {
+      breakName: "La Jolla Shores",
+      regionName: "San Diego — North Side",
+      regionQuickPreview: {
+        waterQuality: "check",
+        suitType: "3/2 Wetsuit",
+        waveSize: "8-12ft",
+        swellDirection: "SSW"
+      },
+      
+      today: {
+        date: "01/01/20",
+        waterQuality: null,
+        moonPhase: null,
+        sunData: {
+          sunRise: null,
+          sunSet: null,
+          firstLight: null,
+          lastLight: null,
+        },        
+        tideData: {
+          highTide: [
+            { tideTime: null,
+              tideHeight: null
+            }
+          ],
+          lowTide: [],
+        },
+        timeSlots: [
+          {
+            time: 4,
+            meridiem: "am",
+            waveSize: null,
+            waveEnergy: null,
+            swellDirection: null, 
+            swellSize: null,
+            swellPeriod: null,
+            tideHeight: null,
+            tideDireciton: null,
+            windSpeed: null,
+            windDirection: null,
+            tempAir: null,
+            tempWater: null,                    
+          }
+        ]
+        
+      },
+      tomorrow: {
+        date: "01/02/20"
+      }
+
+
+    },
+    {}
+  ]
   break1 = {
     name: 'La Jolla Shores'
   }
