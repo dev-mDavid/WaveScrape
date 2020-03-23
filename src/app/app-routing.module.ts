6fa5f19f7a78bffc60bda8c0b6e012b3cdc71a5f
mDavid <dev.mdavid@gmail.com>
2020-03-22 22:08:52 -0700

    generate home page module and component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./home-page/home.component";
 
 
-const routes: Routes = [{ path: 'current', loadChildren: () => import('./current-page/current.module').then(m => m.CurrentModule) }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'current', loadChildren: () => import('./current-page/current.module').then(m => m.CurrentModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
