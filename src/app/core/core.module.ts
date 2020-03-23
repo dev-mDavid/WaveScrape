345eb33fbb9bce9566d917914ace7fb49923ed59
mDavid <dev.mdavid@gmail.com>
2020-03-22 19:39:57 -0700

    generate and config google auth sign-in directive for any button

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { GoogleSigninDirective } from "./user/google-signin.directive";
 
 @NgModule({
-  declarations: [],
+  declarations: [GoogleSigninDirective],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [GoogleSigninDirective]
 })
 export class CoreModule { }
