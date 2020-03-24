d84beefc1a1b7b861dc679b3f8734fa7c53d0720
mDavid <dev.mdavid@gmail.com>
2020-03-23 17:28:36 -0700

    generate admin component to house module components

--- src/app/admin-page/admin.module.ts
+++ src/app/admin-page/admin.module.ts
@@ -6,11 +6,12 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { SharedModule } from "../shared/shared.module";
 import { AdminShellComponent } from './admin-shell/admin-shell.component';
 import { AdminCurrentComponent } from "./admin-components/admin-current/admin-current.component";
+import { AdminComponent } from '../admin/admin.component';
 
 @NgModule({
   declarations: [
     AdminShellComponent, 
-    AdminCurrentComponent],
+    AdminCurrentComponent, AdminComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
